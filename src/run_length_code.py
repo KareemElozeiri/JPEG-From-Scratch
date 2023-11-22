d31232b1cfbf1fa970a1cfaf105d981e2dbcb657
KareemElozeiri <k.elozeiri@gmail.com>
2023-11-23 00:17:52 +0200

    feat(run_length_code): add run length code encoder/decoder

---
+++ src/run_length_code.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+class RunLengthCode:
+    
+    @staticmethod
+    def encode(vector: np.array)->np.array:
+        out_vect = np.array([])
+
+        i = 0
+        while i<len(vector):
+            out_vect = np.append(out_vect, vector[i])
+            i += 1
+            if vector[i-1]==0:
+                count = 1
+                
+                while vector[i]==0:
+                    count += 1
+                    i+= 1
+
+                out_vect = np.append(out_vect ,count)
+
+        return out_vect
+
+
+    @staticmethod
+    def decode(vector: np.array)->np.array:
+        out_vect = np.array([])
+
+        i = 0
+        while i<len(vector):
+            if vector[i] != 0:
+                out_vect = np.append(out_vect, vector[i])
+                i += 1
+            else:
+                out_vect = np.append(out_vect, np.zeros(int(vector[i+1])))
+                i +=2
+
+
+        return out_vect
+
