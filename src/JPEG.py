5930a2d9461a2f0be87e7090b8a7fc84e7c44afd
KareemElozeiri <k.elozeiri@gmail.com>
2023-11-29 21:38:12 +0200

    chore(JPEG): cleaning img2vector & vector2img code

--- src/JPEG.py
+++ src/JPEG.py
@@ -8,7 +8,8 @@ class JPEG:
     @staticmethod
     def img2vector(img:np.array)->np.array:
         rows, cols = img.shape
-        vector = np.array([])
+        vector = np.zeros(rows*cols)
+        current_index = 0
 
         for d in range(rows+cols-1):
             if d%2 == 0:
@@ -17,11 +18,6 @@ class JPEG:
                     d1 = rows - 1
                     d2 = d - d1
                     limit = d1-d2+1
-                for i in range(limit):
-                    vector = np.append(vector, img[d1, d2])
-                    d1 = d1 -1
-                    d2 = d2 +1
-                    
             else:
                 d1, d2, limit = 0, d, d+1
 
@@ -30,12 +26,18 @@ class JPEG:
                     d1 = d - d2
                     limit = d2-d1 +1
 
-                for i in range(limit):
-                    vector = np.append(vector, img[d1, d2])
+
+            for i in range(limit):
+                vector[current_index] = img[d1, d2]
+                current_index += 1
+                if d%2 == 0:
+                    d1 -= 1
+                    d2 += 1
+                else:
                     d1 += 1
                     d2 -= 1         
             
-            return vector
+        return vector
     
     @staticmethod
     def vector2img(vector:np.array, img_shape):
@@ -50,23 +52,20 @@ class JPEG:
                     d1 = rows - 1
                     d2 = d - d1
                     limit = d1-d2+1
-                for i in range(limit):
-                    img[d1, d2] = vector[current_index]
-                    current_index +=1
-                    d1 = d1 -1
-                    d2 = d2 +1
-                    
             else:
                 d1, d2, limit = 0, d, d+1
-
                 if d>rows-1:
                     d2 = rows - 1
                     d1 = d - d2
                     limit = d2-d1 +1
 
-                for i in range(limit):
-                    img[d1, d2] = vector[current_index]
-                    current_index += 1
+            for i in range(limit):
+                img[d1, d2] = vector[current_index]
+                current_index += 1
+                if d%2 == 0:
+                    d1 -= 1
+                    d2 += 1
+                else:
                     d1 += 1
                     d2 -= 1         
         
